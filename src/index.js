00cd47857ad693690baa3746ff8e311498288939
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-02 11:05:29 +0900

    feat: styled component
    bar, graduation, ruler 모두 스타일 적용

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import styled from "styled-components";
 
-const ruleCnt = 6 // 0ms~60ms까지 보여주겠다는 뜻
+// ================= global variable ======================= //
+const ruleCnt = 10 // 0ms~{ruleCnt*10}ms까지 보여주겠다는 뜻
 
-// ================= style =======================
+
+
+// ================= style ======================= //
 const StyledRulergraduation = styled.div`
     position: absolute;
     left: ${(props) => props.i/props.cnt*100}%;
@@ -20,6 +23,15 @@ const StyledRulergraduation = styled.div`
     }
 `;
 
+const StyledBargraduation = styled.div`
+    top: 0%;
+    left: ${(props) => props.i/props.cnt*100}%; 
+    position: absolute;
+    height: 100%;
+    width: 0.5px;
+    background-color: rgb(204, 204, 204);
+`;
+
 const StyledBar = styled.div`
     position: absolute;
     cursor: pointer;
@@ -29,14 +41,16 @@ const StyledBar = styled.div`
     height: 100%;
     width: ${(props) => props.duration/props.cnt*10}%; 
     z-index: 1;
-    background-color: ${(props) => 'rgb(' + props.start*5156%255 + ',' + props.duration*356%255 +',' + ((props.start + props.duration)*35156%255) + ')'}
+    background-color: ${(props) => 'rgb(' + props.start*5156%255 + ',' + props.duration*356%255 +',' + ((props.start + props.duration)*35156%255) + ')'} // 수정 예정
 `;
 
 
-// ================= component =======================
+
+
+// ================= component ======================= //
 class Ruler extends React.Component {
     render() {
-        const mapToGraduation = () => {
+        const mapToRulergraduation = () => { // 줄자 눈금 반복 랜더링
             const result = [];
             for(let i=0; i<ruleCnt*10+1; i++){
                 result.push(<StyledRulergraduation i={i} cnt={ruleCnt*10} key={i}/>)
@@ -48,7 +62,7 @@ class Ruler extends React.Component {
             <div className="ruler-container">
                 <div className="ruler-blank"></div>
                 <div className="ruler">
-                    {mapToGraduation()}
+                    {mapToRulergraduation()}
                 </div>
             </div>
         );
@@ -79,29 +93,15 @@ class DataBar extends React.Component {
         return <Bar idx={ i*10 } duration={ 5 }/>;
     }
 
-    addGraduation(){
-        const graduations = document.querySelectorAll('.data-bar .graduation')
-        const cnt = ruleCnt
-        graduations.forEach(graduation => {
-            for(let i=1; i<cnt; i++){
-                let scale = document.createElement('div')
-                scale.className = 'scale'
-                scale.style.top = '0%'
-                scale.style.left = `${i/ruleCnt*100}%`
-                scale.style.position = 'absolute'
-                scale.style.height = '100%'
-                scale.style.width = '0.5px'
-                scale.style.backgroundColor = 'rgb(204, 204, 204)'
-                graduation.append(scale)
-            }
-        })
-    }
-
-    componentDidMount() {
-        this.addGraduation()
-    }
-
     render() {
+        const mapToBarGraduation = () => {
+            const result = [];
+            for(let i=0; i<ruleCnt; i++){
+                result.push(<StyledBargraduation i={i} cnt={ruleCnt} key={i}/>)
+            }
+            return result
+        }
+
         return (
             <div className="data-bar-container">
                 <header className="data-bar-title"><div>category</div></header>
@@ -113,6 +113,7 @@ class DataBar extends React.Component {
                     {this.renderBar(4)}
                     {this.renderBar(5)}
                     <div className="graduation">
+                        {mapToBarGraduation()}
                     </div>
                 </div>
             </div>
