ecfcb64d7bf645990db092be8ea3dc3d6bda8ab2
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-02 12:59:01 +0900

    feat: Zoomin/ZoomOut prototype
    확대/축소 기능 추가. 추후에 슬라이더 등으로 변경 예정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import styled from "styled-components";
 
-// ================= global variable ======================= //
-const ruleCnt = 10 // 0ms~{ruleCnt*10}ms까지 보여주겠다는 뜻
-
-
-
 // ================= style ======================= //
 const StyledRulergraduation = styled.div`
     position: absolute;
@@ -44,7 +39,9 @@ const StyledBar = styled.div`
     background-color: ${(props) => 'rgb(' + props.start*5156%255 + ',' + props.duration*356%255 +',' + ((props.start + props.duration)*35156%255) + ')'} // 수정 예정
 `;
 
-
+const ZoomInOut = styled.div`
+    width: ${(props) => props.ratio || 100}%;
+`;
 
 
 // ================= component ======================= //
@@ -52,8 +49,8 @@ class Ruler extends React.Component {
     render() {
         const mapToRulergraduation = () => { // 줄자 눈금 반복 랜더링
             const result = [];
-            for(let i=0; i<ruleCnt*10+1; i++){
-                result.push(<StyledRulergraduation i={i} cnt={ruleCnt*10} key={i}/>)
+            for(let i=0; i<this.props.rulerCnt*10+1; i++){
+                result.push(<StyledRulergraduation i={i} cnt={this.props.rulerCnt*10} key={i}/>)
             }
             return result
         }
@@ -70,19 +67,10 @@ class Ruler extends React.Component {
 }
 
 class Bar extends React.Component {
-    constructor(props) {
-        super(props); // 생성자를 가질 때 반드시 작성해야 함
-        this.state = {
-            idx: props.idx,
-            start: props.idx,
-            duration: props.duration,
-        };
-    }
-
     render() {
         return (
-            <StyledBar start={this.state.start} duration={this.state.duration} cnt={ruleCnt}>
-                <div className="bar-title">OP-{this.state.idx}</div>
+            <StyledBar start={this.props.start} duration={this.props.duration} cnt={this.props.cnt}>
+                <div className="bar-title">OP-{this.props.idx}</div>
             </StyledBar>
         );
     }
@@ -90,14 +78,14 @@ class Bar extends React.Component {
 
 class DataBar extends React.Component {
     renderBar(i) {
-        return <Bar idx={ i*10 } duration={ 5 }/>;
+        return <Bar idx={ i*10 } start={i*10} duration={ 5 } cnt={this.props.rulerCnt}/>;
     }
 
     render() {
         const mapToBarGraduation = () => {
             const result = [];
-            for(let i=0; i<ruleCnt; i++){
-                result.push(<StyledBargraduation i={i} cnt={ruleCnt} key={i}/>)
+            for(let i=0; i<this.props.rulerCnt; i++){
+                result.push(<StyledBargraduation i={i} cnt={this.props.rulerCnt} key={i}/>)
             }
             return result
         }
@@ -125,7 +113,10 @@ class Level extends React.Component {
     constructor(props) {
         super(props);
         this.handleLevelClick = this.handleLevelClick.bind(this);
-        this.state = {isPannelOpen: true};
+    }
+
+    state = {
+        isPannelOpen: true,
     }
 
     handleLevelClick(){
@@ -133,6 +124,7 @@ class Level extends React.Component {
     }
 
     render() {
+        // const rulerCnt = this.props.rulerCnt
         return (
             <div className="level-container">
                 <header className="level-title" onClick={this.handleLevelClick}>
@@ -140,8 +132,8 @@ class Level extends React.Component {
                 </header>
                 { this.state.isPannelOpen ?
                     <div className="level-content">
-                        <DataBar/>
-                        <DataBar/>
+                        <DataBar rulerCnt={this.props.rulerCnt}/>
+                        <DataBar rulerCnt={this.props.rulerCnt}/>
                     </div>
                 : ''}
             </div>
@@ -150,15 +142,31 @@ class Level extends React.Component {
 }
 
 class Board extends React.Component {
+    state = {
+        rulerCnt: 6,
+        ratio: 100,
+    }
+
+    handleRulerCntClick(value){
+        this.setState({ratio: this.state.ratio + value})
+    }
+
     render() {
         return (
-        <div className="borad">
-            <div className="content">
-                <Ruler/>
-                <Level/>
-                <Level/>
-                <Level/>
+        <div>
+            <div className="borad">
+                <ZoomInOut ratio={this.state.ratio} className="content">
+                    <Ruler rulerCnt={this.state.rulerCnt}/>
+                    <Level rulerCnt={this.state.rulerCnt}/>
+                    <Level rulerCnt={this.state.rulerCnt}/>
+                    <Level rulerCnt={this.state.rulerCnt}/>
+                </ZoomInOut>
             </div>
+            <div>
+                    Zoom In/Out {this.state.ratio}%
+                    <button onClick={() => this.handleRulerCntClick(1)}>Zoom In +</button>
+                    <button onClick={() => this.handleRulerCntClick(-1)}>Zoom Out -</button>
+                </div>
         </div>
         );
     }
