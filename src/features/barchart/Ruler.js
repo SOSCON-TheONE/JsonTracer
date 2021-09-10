bcde54c72d19f59b5bce9178e3cbcf57fc92cd82
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-10 21:09:45 +0900

    feat: change graduation
    눈금 수를 동적으로 입력합니다. 종료시간의 자릿수에 따라 숫자 뒤에 붙는 시간의 단위를 다르게 표시합니다.
    눈금을 분할하는 갯수도 종료시간에 맞춰 자동으로 변경됩니다.

--- src/features/barchart/Ruler.js
+++ src/features/barchart/Ruler.js
@@ -7,21 +7,37 @@ const StyledRulergraduation = styled.div`
     left: ${(props) => props.i/props.cnt*100}%;
     bottom: 0%;
     width: 0.5px;
-    height: ${(props) => props.i%10 === 0? 100 : 25}%;
+    height: 100%;
     background-color: var(--vscode-foreground);
     &:after {
-        content: '${(props) => props.i%10 === 0 && props.i<props.cnt ? props.i+'ms' : ''}';
+        content: '${(props) => props.graduation}';
         margin-left: 3px;
         font-size: 10px;
     }
 `;
 
 class Ruler extends Component {
+    calculateGraduation(graduation) {
+        if (graduation >= 1000) {
+            return graduation/1000 + 'ms'
+        } else if (graduation >= 1) {
+            return graduation + 'us'
+        } else if (graduation === 0) {
+            return 0
+        } else {
+            return graduation*1000 + 'ns'
+        }
+    }
+
     render() {
         const mapToRulergraduation = () => { // 줄자 눈금 반복 랜더링
             const result = [];
-            for(let i=0; i<this.props.rulerCnt*10+1; i++){
-                result.push(<StyledRulergraduation i={i} cnt={this.props.rulerCnt*10} key={i}/>)
+            for(let i=0; i<parseInt(this.props.calculatedEndTime/(10**(this.props.digit-1))); i++){
+                result.push(<StyledRulergraduation
+                                i={i}
+                                cnt={parseInt(this.props.calculatedEndTime/(10**(this.props.digit-1)))}
+                                graduation={this.calculateGraduation(i*(10**(this.props.digit-1)))}
+                                key={i}/>)
             }
             return result
         }
