a6b864bcb7a71e1ac0f7fd597f4685bbac767bd5
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-10 21:10:57 +0900

    feat: apply displayTimrUnit
    디테일의 ts, dur에 displayTimrUnit 단위를 표시하며, 숫자도 그 단위에 맞게 자릿수를 조정합니다.

--- src/features/barchart/Detail.js
+++ src/features/barchart/Detail.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Detail extends Component {
+    state = {
+        timeUnit: {'ms': 10**(-3), 'us': 1, 'ns': 10**3}
+    }
+
     renderArgs(value){
         return Object.keys(value).map((key) => {
             return <div className="arg" key={key}>ㄴ{key} : {value[key]}</div>
@@ -12,6 +16,8 @@ class Detail extends Component {
             return Object.keys(this.props.selectedOP).map((key) => {
                 if (key === 'args') {
                     return  <div className="args" key={key}>{key} {this.renderArgs(this.props.selectedOP[key])}</div>
+                } else if (key === 'ts' || key === 'dur') {
+                    return  <div key={key}>{key} : {Math.round(this.props.selectedOP[key]*this.state.timeUnit[this.props.displayTimeUnit]*1000)/1000} {this.props.displayTimeUnit} </div>
                 } else {
                     return  <div key={key}>{key} : {this.props.selectedOP[key]}</div>
                 }
